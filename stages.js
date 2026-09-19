982d916e0762c6cb43e9d57c303a5738f93b7087
“munkh-ochir” <munkhluu0608@gmail.com>
2026-09-20 00:04:39 +0800

    Add performance test scripts and results for threshold checks
    - Created `stages.js` for gradual load testing with defined stages and thresholds.
    - Implemented `threshold-fail.js` to test failure scenarios with strict thresholds.
    - Implemented `threshold-pass.js` to test success scenarios with relaxed thresholds.
    - Added result logs for both pass and fail scenarios in `run-threshold-pass.txt` and `run-threshold-fail.txt`.

--- stages.js
+++ stages.js
@@ -2,6 +2,13 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
+  stages: [
+    { duration: '15s', target: 5 },
+    { duration: '15s', target: 30 },
+    { duration: '15s', target: 100 },
+    { duration: '15s', target: 0 },
+  ],
+
   thresholds: {
     http_req_duration: ['p(95)<406.08'],
     http_req_failed: ['rate<0.01'],
--- threshold-fail.js
+++ threshold-fail.js
@@ -2,8 +2,11 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
+  vus: 30,
+  duration: '1m',
+
   thresholds: {
-    http_req_duration: ['p(95)<406.08'],
+    http_req_duration: ['p(95)<50'],
     http_req_failed: ['rate<0.01'],
   },
 };
